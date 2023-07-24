d772c8f61b24d422bd92737d4a4d654431666b26
Arijit Naskar <117929311+Arijitnaskar12@users.noreply.github.com>
2023-07-24 13:00:05 +0000

    Submitted

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[user,setUser]=useState({name:"",email:"",password:""});
+  function Submitted(){
+    console.log(user);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <form onSubmit={Submitted}>
+          <input type="text" placeholder="Name" onChange={(e)=>setUser(...user,name=e.target.value)}/>
+          <input type="email" placeholder="email" onChange={(e)=>setUser(...user,email=e.target.value)}/>
+          <input type="password" placeholder="Password" onChange={(e)=>setUser(...user,password=e.target.value)}/>
+        </form>
     </div>
   )
 }
