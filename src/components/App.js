da683111c4a8f21c51977e59827121972a220b62
Arijit Naskar <117929311+Arijitnaskar12@users.noreply.github.com>
2023-07-24 14:06:34 +0000

    submitted3

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,19 @@ import './../styles/App.css';
 
 const App = () => {
   let[user,setUser]=useState({name:"",email:"",password:""});
-  function Submitted(){
-    console.log(user);
-  }
+  //   console.log(user);
+    function submitted(e){
+      e.preventDefault();
+      console.log(user);
+    }
   return (
     <div>
         {/* Do not remove the main div */}
-        <form onSubmit={Submitted}>
-          <input type="text" placeholder="Name" onChange={(e)=>setUser(...user,name=e.target.value)}/>
-          <input type="email" placeholder="email" onChange={(e)=>setUser(...user,email=e.target.value)}/>
-          <input type="password" placeholder="Password" onChange={(e)=>setUser(...user,password=e.target.value)}/>
+        <form>
+          <input type="text" placeholder="Name" value={user.name} onChange={(e)=>setUser({...user,name:e.target.value})}/>
+          <input type="email" placeholder="email" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})}/>
+          <input type="password" placeholder="Password" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})}/>
+          <button onClick={submitted}>Submit</button>
         </form>
     </div>
   )
